2e319af10317eea6a2fce5ff40e3b524188c7dc8
Jordan <jordanramey101@yahoo.com>
2025-12-02 14:18:36 -0800

    lots of new assignments

--- excercises/ex04.js
+++ excercises/ex04.js
@@ -1,24 +1,81 @@
+var count = 0;
+let colorCount=0;
+var colors = ["Orchid", "Coral", "HotPink", "Plum"]; 
 
-let totalNumber = 0;
 
-// add a button titled click me
+$("#needy-button").click(function () {
 
-$("#needy-button").click (function (){
-    
-    totalNumber = totalNumber +3;
+    if ( count < 5) { mood = "gresh and happy" ; }
+    else if ( (count >=5) && (count <10 )) { mood="keep pushing"; }
+    else { mood = "so tired" ; }
 
-let sentence = "I was clicked";
-let totalNumbers= sentence + totalNumber;
+    $("#needy-button").html( "Clicks: " + count + " Color: " + colors[colorCount] + " " + mood); 
 
-$("#needy-button").html (totalNumbers);
-   
-})
+    $("body").css("background-color", colors[colorCount]);
 
-// when it is clicked
-// add one to the total number
+    if ( colors[colorCount] == "Orchid" ) {
+        $("body").append("<img width=50 src='images/orchid.png'>");
+}
 
-// show the total number
-// on our button
-// show "total number times clicked"
 
-// a top limit
+
+    count = count + 1;
+    colorCount = colorCount + 1;
+
+    if ( colorCount == 4 ) {  colorCount=0;  }
+
+
+
+
+});
+
+
+
+
+
+// let count = 0;
+// let colorIndex = 0;
+// let mood;
+// let colors = ["Orchid", "Coral", "HotPink", "Plum"];
+
+// $("#needy-button").click(function () {
+
+//     if (count < 5) { mood = "fresh and happy"; }
+//     else if ( count > 3) { mood = "keep pushing"; }
+//     else { mood = "so tired!"; }
+
+//     $("#needy-button").html("Clicks: " + count + " Color: " + colors[colorIndex] + " - " + mood);
+
+//     $("body").css("background-color", colors[colorIndex]);
+
+//     count = count + 1;
+//     colorIndex = colorIndex + 1;
+
+//     if (colorIndex == 4) { colorIndex = 0; } // new line
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+// let count = 0;
+// let colors = ["Orchid", "Coral", "HotPink", "Plum"]; 
+
+// $("#needy-button").click(function () {
+
+//     $("#needy-button").html( "Clicks: " + count + " Color: " + colors[count]  + " ...fresh and happy" ); 
+
+//     $("body").css("background-color", colors[count]);
+
+//     count = count + 1;
+
+//     if (count==4) { count=0; } // new line
+
+// });
---
+++ excercises/lab4.js
@@ -0,0 +1,10 @@
+// declaring an array with name myCommutes
+let myCommutes = "metro bus"
+// declaring an object with name myFavouriteCommute
+let myFavouriteCommute = {
+    type: "Bus";
+    route: 35, 19, 18, 11;
+    print: "Sea Otter",
+    hasMiddleDoor: true,
+    drivers: ["the lady with cat ears", "the guy with the brown beard and hat"],
+};
